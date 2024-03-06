1b533f507fd741fe9e1b80e89cfd8d11ad8070f6
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-03-06 13:47:18 -0300

    First Commit

---
+++ MiniChallenge04/MiniChallenge04/MiniChallenge04App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge04App.swift
+//  MiniChallenge04
+//
+//  Created by Guilherme Nunes Lobo on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
