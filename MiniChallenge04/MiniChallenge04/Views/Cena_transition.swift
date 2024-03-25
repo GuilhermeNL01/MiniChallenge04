effba2a04a58549b8fcc5769dd9d17b4987a3b7b
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-03-25 16:56:20 -0300

    Animation

--- MiniChallenge04/MiniChallenge04/Views/Cena_transition.swift
+++ MiniChallenge04/MiniChallenge04/Views/Cena_transition.swift
@@ -8,7 +8,45 @@
 import SpriteKit
 
 class CenaTransition: SKScene {
+    
+    var characterNode: SKSpriteNode!
+    var cameraNode: SKCameraNode!
+    var background: SKSpriteNode!
+    var dialogos: [DialogueBox] = []
+    
     override func didMove(to view: SKView) {
-        self.backgroundColor = .red
+        background = SKSpriteNode(imageNamed: "Background")
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.size = self.size
+        addChild(background)
+        
+        characterNode = SKSpriteNode(imageNamed: "character")
+        characterNode.position = CGPoint(x: frame.midX - 200, y: frame.midY)
+        addChild(characterNode)
+        
+        cameraNode = SKCameraNode()
+        camera = cameraNode
+        addChild(cameraNode)
+        self.camera = cameraNode
+        
+        cameraNode.position = CGPoint(x: frame.midX, y: frame.midY)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let touchedNode = self.atPoint(location)
+            
+            if touchedNode == characterNode {
+                let zoomInAction = SKAction.scale(to: 0.8, duration: 1.0)
+                cameraNode.run(zoomInAction)
+                
+                let centerPosition = CGPoint(x: background.position.x, y: background.position.y)
+                let moveAction = SKAction.move(to: centerPosition, duration: 1.0)
+                characterNode.run(moveAction)
+                
+                
+            }
+        }
     }
 }
