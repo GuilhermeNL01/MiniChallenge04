c2e82c162ba613a779e30275e58ea9add5b02f7b
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-04-01 17:38:35 -0300

    Adjusts

--- MiniChallenge04/MiniChallenge04/Views/Map/Map.swift
+++ MiniChallenge04/MiniChallenge04/Views/Map/Map.swift
@@ -11,8 +11,8 @@ import SwiftUI
 class Map: SKScene{
     var background = SKSpriteNode(imageNamed: "MapBackground")
     var hotel = SKSpriteNode(imageNamed: "HotelAvailable")
-    var pier = SKSpriteNode(imageNamed: "PierImage")
-    var butcher = SKSpriteNode(imageNamed: "ButcherImage")
+    var pier = SKSpriteNode(imageNamed: "PierAvailable")
+    var butcher = SKSpriteNode(imageNamed: "ButcherAvailable")
     
     var myView: SKView = SKView()
     var nextScene: SKScene?
