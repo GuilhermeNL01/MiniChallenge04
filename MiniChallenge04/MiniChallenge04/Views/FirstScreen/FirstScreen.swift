96603a0c425c85edaf220cdf182c7d8df038b108
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-04-02 14:18:23 -0300

    Navigate

--- MiniChallenge04/MiniChallenge04/Views/FirstScreen/FirstScreen.swift
+++ MiniChallenge04/MiniChallenge04/Views/FirstScreen/FirstScreen.swift
@@ -41,7 +41,7 @@ struct FirstScreen: View {
                         Spacer()
                         
                         Button{
-                            path.append(VideoCutscene2(path: $path))
+                            path.append(VideoCutsceneScene(path: $path))
                         } label: {
                             Image("Start")
                                 .resizable()
