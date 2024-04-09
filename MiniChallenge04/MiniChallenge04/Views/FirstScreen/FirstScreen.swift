5d5b7f82b9c950d4c32f926f816a2460ca6d9c87
Enrique-mac <carvalhoenrique33@gmail.com>
2024-04-09 15:39:50 -0300

    small fixes

--- MiniChallenge04/MiniChallenge04/Views/FirstScreen/FirstScreen.swift
+++ MiniChallenge04/MiniChallenge04/Views/FirstScreen/FirstScreen.swift
@@ -38,7 +38,7 @@ struct FirstScreen: View {
                         
                         Button{
                             withAnimation{
-                                path.append(VideoCutsceneScene(path: $path))
+                                path.append(HotelScene(path: $path))
                             }
                         } label: {
                             Image("Start")
